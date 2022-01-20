67ff7eeaa34768d911beb60d464bf21d4946379a
AndreyMandybura <80692584+AndreyMandybura@users.noreply.github.com>
2022-01-20 14:13:50 +0200

    Сообщение по действию с контактом + проверка на пустые поля при добавлении

--- README.md
+++ README.md
@@ -1,19 +1,19 @@
 # Получаем и выводим весь список контактов в виде таблицы (console.table)
 
 node index.js --action list
-https://monosnap.com/file/oy0fbNWw8WW4j5rzu0TWKEuPkvCIVn
+https://monosnap.com/file/6SelmEXetoKWIKuccclOyOYXDFjbYP
 
 # Получаем контакт по id
 
 node index.js --action get --id 5
-https://monosnap.com/file/qreZWm145onYxxdkfGspVeEoEAoLei
+https://monosnap.com/file/yKTWTa0ew8PMM5OKsYsMDzuIH8WXhC
 
 # Добавялем контакт
 
 node index.js --action add --name Mango --email mango@gmail.com --phone 322-22-22
-https://monosnap.com/file/xAY44FXUbaH7pxbxO7Gv5wLalPTkcx
+https://monosnap.com/file/Htk2vMFihWtwLsRtdgd1q3pOS9Wugk
 
 # Удаляем контакт
 
 node index.js --action remove --id=3
-https://monosnap.com/file/0EABLtg3j6TnsdM1MFlNoGXWXeFTZS
+https://monosnap.com/file/EmMmDVAjRCkQDlgUOxxHYnivWnt5Zd
--- index.js
+++ index.js
@@ -15,22 +15,30 @@ const argv = program.opts();
 async function invokeAction({ action, id, name, email, phone }) {
   switch (action) {
     case 'list':
-          const contacts = await contactsOperations.listContacts();
+      const contacts = await contactsOperations.listContacts();
+          console.log('\nYour contacts:');
           console.table(contacts);
       break;
 
     case 'get':
       const contact = await contactsOperations.getContactById(id);
+          console.log('\nFound next contact:');
           console.log(contact);
       break;
 
     case 'add':
-          const newContact = await contactsOperations.addContact(name, email, phone);
+      if (!name || !email || !phone) {
+            console.log('\nThe contact could not be added. Please complete all fields.')
+            break
+        }
+      const newContact = await contactsOperations.addContact(name, email, phone);
+          console.log('\nAdded next contact:');
           console.log(newContact);
       break;
 
     case 'remove':
       const removeContact = await contactsOperations.removeContact(id);
+          console.log('\nRemoved next contact:');
           console.log(removeContact);
       break;
 
